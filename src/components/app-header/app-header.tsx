3916c9af2a7962eb51483edcf0d612134b21cc60
ZuevaDarya <zuevadasha55@gmail.com>
2024-11-15 18:09:15 +0300

    [REFACTOR]: Поправила стили для шапки

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -13,16 +13,16 @@ function AppHeader() {
     <header className="header pt-4 pb-4">
       <nav className="header-nav">
         <NavList>
-          <NavListItem linkText="Конструктор" linkHref="#" isActive={true}>
+          <NavListItem linkText="Конструктор" linkHref="#" isActive={true} classes="pr-5">
             <BurgerIcon type="primary" className="nav-icon_primary" />
           </NavListItem>
-          <NavListItem linkText="Лента заказов" linkHref="#" isActive={false}>
+          <NavListItem linkText="Лента заказов" linkHref="#" isActive={false} classes="pl-5 pr-5">
             <ListIcon type="disabled" className="nav-icon_disabled" />
           </NavListItem>
         </NavList>
         <Logo />
         <NavList>
-          <NavListItem linkText="Личный кабинет" linkHref="#" isActive={false}>
+          <NavListItem linkText="Личный кабинет" linkHref="#" isActive={false} classes="pl-5">
             <ProfileIcon type="disabled" className="nav-icon_disabled" />
           </NavListItem>
         </NavList>
--- src/components/nav-list-item/nav-list-item.tsx
+++ src/components/nav-list-item/nav-list-item.tsx
@@ -4,10 +4,17 @@ type NavLinkPropsType = {
   linkText: string;
   linkHref: string;
   isActive: boolean;
+  classes?: string;
   children?: React.ReactNode;
 };
 
-function NavListItem({ linkText, linkHref, isActive, children}: NavLinkPropsType) {
+function NavListItem({
+  linkText,
+  linkHref,
+  isActive,
+  classes,
+  children,
+}: NavLinkPropsType) {
   const LINK_TYPES = {
     default: "nav-link_default",
     disabled: "nav-link_disabled",
@@ -19,7 +26,7 @@ function NavListItem({ linkText, linkHref, isActive, children}: NavLinkPropsType
   }
 
   return (
-    <li className="nav-list-item pt-4 pb-4 pl-5 pr-5">
+    <li className={`nav-list-item pt-4 pb-4 ${classes}`}>
       {children}
       <a
         className={`text text_type_main-default nav-link ${linkActiveClass}`}
