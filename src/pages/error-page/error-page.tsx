1357bc851ac54cf9759fff913f0e8f367e8bcda9
ZuevaDarya <zuevadasha55@gmail.com>
2024-12-16 15:45:36 +0300

    [FEAT]: наверстала страницу ошибки

--- src/pages/error-page/error-page.tsx
+++ src/pages/error-page/error-page.tsx
@@ -1,9 +1,30 @@
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import errorStyles from "./error-page.module.css";
+import { useNavigate } from "react-router-dom";
+import { AppRoute } from "../../constants/app-route";
+
 function ErrorPage() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(AppRoute.Home, { replace: true });
+  };
+
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-    </div>
+    <main id="error-page">
+      <div className={errorStyles.container}>
+        <h1 className="text text_type_digits-large">404</h1>
+        <p className="text text_type_main-medium">
+          Упсс... Такая страница не найдена
+        </p>
+        <p className="text text_type_main-default">
+          Вы можете вернуться на страницу с конструтором и собрать лучший
+          галактический бургер
+        </p>
+        <Button htmlType="button" onClick={handleClick}>
+          На главную
+        </Button>
+      </div>
+    </main>
   );
 }
 
